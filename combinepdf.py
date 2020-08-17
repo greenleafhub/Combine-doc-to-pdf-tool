de8122ab8803c0964f1bc40c6653ae08ab6ac2f7
greenleafhub <leung_ht@yahoo.com.hk>
2020-08-17 17:33:18 +0800

    Combine pdf and make page number even
    Combine all pdf pages in the same directory, and add a blank page if pdf has odd number pages (put a blank pdf file starting with 'blank' in the same directory)
    A blank A4 pdf page is included for the above purpose

---
+++ combinepdf.py
@@ -0,0 +1,43 @@
+   #! python3
+   # combinepdf.py - Combines all the PDFs in the current working directory into
+   # into a single PDF.
+
+import PyPDF2, os
+
+# Get all the PDF filenames.
+pdfFiles = []
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+
+pdfFiles.sort(key = str.lower)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+# Loop through all the PDF files.
+for filename in pdfFiles:
+    pdfFileObj = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+# Create the blank page located in the same directory.
+    if filename.startswith('blank'):
+        blank = pdfReader.getPage(0)
+    if not filename.startswith('blank'):
+        print('Opened ',filename)
+# Loop through all the pages and add them.
+        for pageNum in range(pdfReader.numPages):
+            pageObj = pdfReader.getPage(pageNum)
+            pdfWriter.addPage(pageObj)
+            print('Combining ',filename, (pageNum +1), '...')
+
+        totalpage = int(pdfReader.numPages)
+        if totalpage % 2 == 1:
+            pdfWriter.addPage(blank)
+    else:
+        pass
+
+
+# Save the resulting PDF to a file.
+pdfOutput = open('allminutes.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
+print('Combined all to allminutes.pdf \o/')
+
